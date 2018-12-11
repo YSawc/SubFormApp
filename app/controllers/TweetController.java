50777a54e56db5be3326c293085b515a2ae2a9b0
y-s678 <niceage6690@gmail.com>
2018-12-11 21:26:07 +0900

    Before Change Controller name

--- app/controllers/TweetController.java
+++ app/controllers/TweetController.java
@@ -1,4 +1,15 @@
 package controllers;
 
-public class TweetController {
+import play.data.FormFactory;
+import play.mvc.Controller;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class TweetController extends Controller {
+    @Inject
+    private FormFactory formFactory;
+
+
 }
