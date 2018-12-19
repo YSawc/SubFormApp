e4dd9a635b55fc80b87f061f00e68eae8f68ec01
y-s678 <niceage6690@gmail.com>
2018-12-20 07:49:00 +0900

    TODO SQL

--- app/controllers/FollowsController.java
+++ app/controllers/FollowsController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlRow;
 import models.Follow;
 import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+//import views.html.follows.*;
+import views.html.users.show;
 
 import javax.inject.Singleton;
 import java.util.List;
@@ -15,6 +20,8 @@ public class FollowsController extends Controller {
     //フォローした側はフォローモデルにフォローされた側の名前をセットする
 
     public Result follow(Integer id){
+
+        return TODO;
 
         User user_beFollowed = User.find.byId(id);
         User user_doneFollow = User.find.byId(Integer.parseInt(session("id")));
@@ -36,6 +43,10 @@ public class FollowsController extends Controller {
 
         System.out.println(list + "  list表示");
 
+        String sql = "SELECT follow_id, be_followed_id FROM follow WHERE follow_id='2' AND be_followed_id='1'";
+
+
+        List<SqlRow> sqlRows = Ebean.createSqlQuery(sql).findList();
 
 //        follow.setFollower_id(user_doneFollowed.id);
 //        follow.setFollowed_id(user_beFollowed.id);
@@ -49,9 +60,12 @@ public class FollowsController extends Controller {
         user_doneFollow.setFollow(follow);
         follow.save();
 
+        System.out.println(sql);
+
 //        System.out.println(list + "  list表示");
 
-        return redirect(routes.TweetsController.index());
+//        return ok(followShow.render(sqlRows));
+        return TODO;
     }
 
     public Result show(Integer id){
