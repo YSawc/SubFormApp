b03f4f7e721de558814e8f117c842585f94d247f
y-s678 <niceage6690@gmail.com>
2018-12-20 10:05:31 +0900

    TODO SQL

--- app/controllers/FollowsController.java
+++ app/controllers/FollowsController.java
@@ -11,6 +11,7 @@ import play.mvc.Result;
 import views.html.users.show;
 
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
@@ -21,51 +22,43 @@ public class FollowsController extends Controller {
 
     public Result follow(Integer id){
 
-        return TODO;
-
         User user_beFollowed = User.find.byId(id);
         User user_doneFollow = User.find.byId(Integer.parseInt(session("id")));
-        System.out.println(user_beFollowed.getName() + "フォローされたユーザーの名前");
-        System.out.println(user_doneFollow.getName() + "フォローをしたユーザーの名前");
 
         Follow follow = new Follow();
+
+//        String sql = "SELECT DISTINCT follow_id, be_followed_id FROM follow WHERE follow_id='2' AND be_followed_id='1'";
+        String sql = "SELECT DISTINCT be_followed_id FROM follow WHERE follow_id= "
+                + user_doneFollow.id + " AND be_followed_id=" +user_beFollowed.id ;
+
+        System.out.println(sql + "sql出力");
+
+        //sqlをif分制御
+
+//        if(List<SqlRow>.Contain){
+//
+//        }
 
         follow.setFollow_id(user_doneFollow.id);
         follow.setBeFollowed_id(user_beFollowed.id);
 
-        //実験
-        follow = Follow.find.ref(user_doneFollow.id);
-
-        List<Follow> list = Follow.find.where()
-                .eq("follow_id", user_doneFollow.id)
-                .setDistinct(true).select("followed_id")
-                .findList();
-
-        System.out.println(list + "  list表示");
-
-        String sql = "SELECT follow_id, be_followed_id FROM follow WHERE follow_id='2' AND be_followed_id='1'";
 
 
         List<SqlRow> sqlRows = Ebean.createSqlQuery(sql).findList();
 
-//        follow.setFollower_id(user_doneFollowed.id);
-//        follow.setFollowed_id(user_beFollowed.id);
-
-//        follow.follow_id = user_doneFollow.id;
-//        follow.relationships_id = user_beFollowed.id;
+        List<String> tables = new ArrayList<String>();
 
 //        System.out.println(follow + "リレーションシップのユーザー所有？");
 
-        user_doneFollow.save();
-        user_doneFollow.setFollow(follow);
+//        user_doneFollow.save();
+//        user_doneFollow.setFollow(follow);
         follow.save();
 
-        System.out.println(sql);
+        System.out.println(sqlRows + " sqpRowの出力");
 
 //        System.out.println(list + "  list表示");
 
-//        return ok(followShow.render(sqlRows));
-        return TODO;
+        return redirect(routes.TweetsController.index());
     }
 
     public Result show(Integer id){
